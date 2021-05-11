725e3f714ad5690be3770319c412dac3af647f85
Jaime Cascante <je.cascante10@uniandes.edu.co>
2021-05-11 09:18:47 -0400

    updating bog_rt_estimate

--- bogota_rt_estimate.py
+++ bogota_rt_estimate.py
@@ -35,8 +35,9 @@ data_cases_path = os.path.join(fb_ppl_data, 'agglomerated', 'geometry')
 cases_df = pd.read_csv(os.path.join(data_cases_path, 'cases.csv'), parse_dates=["date_time"])
 cases_bog_df = cases_df.groupby('date_time').sum()[["num_cases", "num_diseased"]]
 cases_bog_df = cases_bog_df.resample('1D').sum().reset_index().rename(columns={'date_time': 'date', 'num_cases': 'confirm', 'num_diseased': 'death'})
-
-r_df_bogota_loc = ro.conversion.py2rpy(cases_bog_df[['date','confirm']])
+cases_bog_df = cases_bog_df.iloc[-14:]
+with localconverter(ro.default_converter + pandas2ri.converter):
+    r_df_bogota_loc = ro.conversion.py2rpy(cases_bog_df[['date','confirm']])
 r_df_bogota_loc[0]  = base.as_Date(r_df_bogota_loc[0], format= "%Y-%m-%d")
 bogota_rt = epinow2.epinow(reported_cases = r_df_bogota_loc,
                     generation_time = generation_time,
@@ -44,6 +45,7 @@ bogota_rt = epinow2.epinow(reported_cases = r_df_bogota_loc,
                     rt = epinow2.rt_opts(prior = ro.r.list(mean = 2, sd = 0.2)),
                     stan = epinow2.stan_opts(cores = 4))#, verborse=ro.r.TRUE)
 
+l='bog'
 inf_df = pd.DataFrame( bogota_rt[0][0] )
 inf_df.to_csv( os.path.join(results_dir, 'bog_rt', 'infections_df_{}_confirmation.csv'.format(l)) )
 rt_df  = pd.DataFrame( bogota_rt[0][1] )
