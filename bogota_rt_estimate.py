3f9887e54f26ed1becaef8159a8f404485dd7ec1
Jaime Cascante <je.cascante10@uniandes.edu.co>
2021-05-11 09:14:37 -0400

    adding Rt estimate Script

--- bogota_rt_estimate.py
+++ bogota_rt_estimate.py
@@ -27,4 +27,24 @@ reporting_delay   = epinow2.estimate_delay(ro.r.rlnorm(1000,  ro.r.log(3), 1),
 
 data_dir_mnps = config.get_property('geo_dir')
 data_dir      = config.get_property('data_dir')
+fb_ppl_data   = config.get_property('covid_fb')
 results_dir   = config.get_property('results_dir')
+
+data_cases_path = os.path.join(fb_ppl_data, 'agglomerated', 'geometry')
+
+cases_df = pd.read_csv(os.path.join(data_cases_path, 'cases.csv'), parse_dates=["date_time"])
+cases_bog_df = cases_df.groupby('date_time').sum()[["num_cases", "num_diseased"]]
+cases_bog_df = cases_bog_df.resample('1D').sum().reset_index().rename(columns={'date_time': 'date', 'num_cases': 'confirm', 'num_diseased': 'death'})
+
+r_df_bogota_loc = ro.conversion.py2rpy(cases_bog_df[['date','confirm']])
+r_df_bogota_loc[0]  = base.as_Date(r_df_bogota_loc[0], format= "%Y-%m-%d")
+bogota_rt = epinow2.epinow(reported_cases = r_df_bogota_loc,
+                    generation_time = generation_time,
+                    delays = epinow2.delay_opts(incubation_period, reporting_delay),
+                    rt = epinow2.rt_opts(prior = ro.r.list(mean = 2, sd = 0.2)),
+                    stan = epinow2.stan_opts(cores = 4))#, verborse=ro.r.TRUE)
+
+inf_df = pd.DataFrame( bogota_rt[0][0] )
+inf_df.to_csv( os.path.join(results_dir, 'bog_rt', 'infections_df_{}_confirmation.csv'.format(l)) )
+rt_df  = pd.DataFrame( bogota_rt[0][1] )
+rt_df.to_csv( os.path.join(results_dir, 'bog_rt', 'rt_df_{}_confirmation.csv'.format(l)) )
