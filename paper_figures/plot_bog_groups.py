4f097dc33824af661587ec053c80558b60895ae6
Jaime Cascante Vega <je.cascante10@uniandes.edu.co>
2024-12-06 17:46:47 -0500

    update bogota covid lockdownS

--- paper_figures/plot_bog_groups.py
+++ paper_figures/plot_bog_groups.py
@@ -1,55 +1,53 @@
-import itertools
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from tqdm import tqdm
+from datetime import timedelta
+import contextily as ctx
 import geopandas as gpd
 from shapely import wkt
 import geopandas as gpd
-import contextily as ctx
-from datetime import timedelta
-import matplotlib.pyplot as plt
+from tqdm import tqdm
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import itertools
+
 from shapely.geometry import Point, LineString
-import os, sys
 from global_config import config
+import matplotlib.pyplot as plt
+import os, sys
 
 geo_dir       = config.get_property('geo_dir')
 results_dir   = config.get_property('results_dir')
 
 path_to_bog_polygon = os.path.join(geo_dir, 'localities', 'localities_shapefile.shp')
 
-
 # Read bogota polygons divided by localities.
 polygons_bog = gpd.read_file( path_to_bog_polygon )
 polygons_bog = polygons_bog.rename(columns={'location_i': 'location_id'})
 
-g1 = ['Chapinero', 'Los Mártires', 'San Cristóbal', 'Tunjuelito',
-    'Rafael Uribe Uribe', 'Ciudad Bolívar', 'Santa Fe', 'Usme']
-g2 = ['Bosa', 'Antonio Nariño', 'Kennedy', 'Puente Aranda', 'Fontibón']
-g3 = ['Suba', 'Engativá', 'Barrios Unidos']
-g4 = ['Usaquén', 'Chapinero', 'Santa Fe', 'La Candelaria', 'Teusaquillo', 'Puente Aranda', 'Antonio Nariño']
-
+g1           = ['Chapinero', 'Los Mártires', 'San Cristóbal', 'Tunjuelito',
+              'Rafael Uribe Uribe', 'Ciudad Bolívar', 'Santa Fe', 'Usme']
+g2           = ['Bosa', 'Antonio Nariño', 'Kennedy', 'Puente Aranda', 'Fontibón']
+g3           = ['Suba', 'Engativá', 'Barrios Unidos']
+g4           = ['Usaquén', 'Chapinero', 'Santa Fe', 'La Candelaria', 'Teusaquillo', 'Puente Aranda', 'Antonio Nariño']
 dict_correct = {'Los Martires': 'Los Mártires', 'Fontibon': 'Fontibón', 'Engativa': 'Engativá',
                             'San Cristobal': 'San Cristóbal', 'Usaquen': 'Usaquén',
                             'Ciudad Bolivar': 'Ciudad Bolívar', 'Candelaria': 'La Candelaria'}
-
 polygons_bog['label'] = polygons_bog['label'].replace(dict_correct)
 
-polygons_bog["group_1"] = None
+polygons_bog["group_1"]       = None
 polygons_bog["group_1_label"] = None
 
-polygons_bog["group_2"] = None
+polygons_bog["group_2"]       = None
 polygons_bog["group_2_label"] = None
 
 polygons_bog["group_3"] = None
 polygons_bog["group_3_label"] = None
 
-polygons_bog["group_4"] = None
+polygons_bog["group_4"]       = None
 polygons_bog["group_4_label"] = None
 
 for loc in g1:
-    polygons_bog["group_1"][polygons_bog.label==loc]=True
-    polygons_bog["group_1_label"][polygons_bog.label==loc]=loc
+    polygons_bog["group_1"][polygons_bog.label==loc]       = True
+    polygons_bog["group_1_label"][polygons_bog.label==loc] = loc
 
 for loc in g2:
     polygons_bog["group_2"][polygons_bog.label==loc]=True
@@ -65,6 +63,7 @@ for loc in g4:
 
 def plot_bog_specified_column(df_polygons, title, col_name='value', cmap = 'seismic_r', labels = True, path_to_save = None):
     fig, ax = plt.subplots(1,1, figsize=(10, 20))
+
     maps1 = polygons_bog.plot(ax=ax, edgecolor='black', alpha=0.3, facecolor='Grey', linewidth=1)
     maps2 = polygons_bog.plot(ax=ax, edgecolor='gray', column=col_name, cmap=cmap, alpha=0.8, linewidth=1)
 
